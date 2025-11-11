322d281324a9c247f05c0ec2677ad0db48e17d57
Idhana7 <85846467+Idhana7@users.noreply.github.com>
2025-11-11 09:57:47 -0600

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,1 +1,31 @@
+public class Persona {
+
+    private String nombre;      // Solo accesible dentro de esta clase
+    protected int edad;         // Accesible en subclases y mismo paquete
+    String correo;              // Acceso por paquete (default)
+    public String ocupacion;    // Accesible desde cualquier parte
+
+    public Persona(String nombre, int edad, String correo, String ocupacion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+        this.ocupacion = ocupacion;
+    }
+
+    private void mostrarNombre() {
+        System.out.println("Nombre: " + nombre);
+    }
+
+    protected void mostrarEdad() {
+        System.out.println("Edad: " + edad);
+    }
+
+    void mostrarCorreo() {
+        System.out.println("Correo: " + correo);
+    }
+
+    public void mostrarOcupacion() {
+        System.out.println("Ocupación: " + ocupacion);
+    }
+}
 
